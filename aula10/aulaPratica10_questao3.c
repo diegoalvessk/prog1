70fccca20ebb2636b7a79b301b9efe19a41f215e
Diego <diegoteialves@gmail.com>
2025-12-11 16:44:10 -0300

    Adicionando questao 3 aula 10

--- aula10/aulaPratica10_questao3.c
+++ aula10/aulaPratica10_questao3.c
@@ -4,67 +4,38 @@
 
 int main() {
     char nome[61];
-    int i, len = 0;
 
     printf("Digite o nome completo: ");
     fgets(nome, 61, stdin);
 
-    for (i = 0; nome[i] != '\0'; i++) {
+    int i = 0;
+    while(nome[i] != '\0'){
         if (nome[i] == '\n') {
             nome[i] = '\0';
             break;
         }
-        len++;
+        i++;
     }
 
-    int totalLetras = 0;
-    for (i = 0; nome[i] != '\0'; i++) {
-        if (nome[i] != ' ') {
-            totalLetras++;
-        }
+    int tam_sobrenome = -1, j;
+    for(j = i; nome[j] != ' '; j--){
+        tam_sobrenome++;
     }
 
-    int ultimoEspaco = -1;
-    for (i = 0; nome[i] != '\0'; i++) {
-        if (nome[i] == ' ') {
-            ultimoEspaco = i;
-        }
+
+    int qtd_espacos = 0;
+    for(int k = j + 1; k < i; k++){
+        printf("%c", nome[k]);
     }
-
-    if (ultimoEspaco == -1) {
-        int letrasUltimo = 0;
-        for (i = 0; nome[i] != '\0'; i++) {
-            if (nome[i] != ' ') letrasUltimo++;
-        }
-
-        printf("\n%s, %s\n", nome, "");
-        printf("\nTotal de letras: %d\n", totalLetras);
-        printf("\nTotal de letras do último sobrenome: %d\n", letrasUltimo);
-        return 0;
-    }
-
-    int letrasUltimo = 0;
-    for (i = ultimoEspaco + 1; nome[i] != '\0'; i++) {
-        if (nome[i] != ' ') {
-            letrasUltimo++;
-        }
-    }
-
-    printf("\n");
-
-    for (i = ultimoEspaco + 1; nome[i] != '\0'; i++) {
-        putchar(nome[i]);
-    }
-
     printf(", ");
 
-    for (i = 0; i < ultimoEspaco; i++) {
-        putchar(nome[i]);
+    for(int k = 0; k <= j; k++){
+        if(nome[k] == ' ') qtd_espacos++;
+        printf("%c", nome[k]);
     }
 
-    printf("\n");
-    printf("\nTotal de letras: %d\n", totalLetras);
-    printf("\nTotal de letras do último sobrenome: %d\n", letrasUltimo);
 
+    printf("\nTotal de letras: %d\nTotal de letras do último sobrenome: %d\n", i - qtd_espacos, tam_sobrenome);
+    
     return 0;
 }
